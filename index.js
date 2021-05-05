397290bc4dd4252f834df9a95c9491fd32b64926
sumonacse <ssria.cse@gmail.com>
2021-05-05 20:01:26 +0600

    Person object create

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+let person = {
+    name: "sifatkazi",
+    isMarried : true,
+    age : 21,
+    friend : [ 
+         'sifz', 'john'
+    ]
+}
+
+console.log(person);
+console.log(person.age);
+console.log(person.friend);
+console.log(person.friend[0]);
+console.log(person.isMarried);
+console.log("Name is: "+person.name);
